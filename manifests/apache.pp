568973a09844784ffc1c2a8b1d70b7f83a529db6
root <root@pmontesd1.mylabserver.com>
2017-09-23 00:05:55 +0000

    remove useless files and links

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,1 +1,16 @@
-apache.pp.exercise
+class profile::apache (
+  Boolean $default_vhost = false,
+  Hash $apache_vhost_servers,
+  Integer $port = 80,
+  String $docroot= '/var/www/test'
+) {
+  class { '::apache':
+    default_vhost => $default_vhost,
+  }
+
+  create_resources(::apache::vhost, $apache_vhost_servers)
+  #::apache::vhost { 'pmontesd1.mylabserver.com':
+  #  port    => $port,
+  #  docroot => $docroot,
+  #}
+}
