524dbd2db659fd332fc73d414cbdcb85cfa9367b
Pablo Montesdeoca <pmontesd@gmail.com>
2017-09-24 21:02:23 +0000

    Use hiera data

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,7 +1,10 @@
 class profile::nginx {
   include ::nginx
 
-  ::nginx::vhost { $facts['fqdn']:
-
+  $vhosts.each |$vhost| {
+    ::nginx::vhost { $vhost['fqdn']:
+      port           => $vhost['port'],
+      server_aliases => $vhost['server_aliases'],
+    }
   }
 }
